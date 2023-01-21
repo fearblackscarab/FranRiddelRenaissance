9b13768d5c7e99347070be6aaba02f998949aa99
fearblackscarab <fearblackscarab@gmail.com>
2023-01-21 10:33:38 -0600

    trying fix for images

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,8 @@ import "./home.css";
 const Home=()=>{
     return(
         <div className="home">
-            <h2 className="test"><a href="https://www.icloud.com/notes/02f9nneHjoGwqUvBrf753RDNg" className="text-center d-block story">Our Story</a></h2>
-            <img src="../public/new_media/white_dress.png" alt="woman in a white dress displaying a bracelet" className="img-fluid"/>
+            <h2 className="test"><a href="" className="text-center d-block story">Our Story</a></h2>
+            <img src="/media/white_dress.png" alt="woman in a white dress displaying a bracelet" className="img-fluid"/>
         </div>
     )
 };
--- src/pages/samples/Samples.jsx
+++ src/pages/samples/Samples.jsx
@@ -7,12 +7,12 @@ const Samples=()=>{
             <h2 className="test">Samples</h2>
             <div className="container">
                 <div className="row">
-                    <Product img='../public/new_media/IMG_9687.jpg' name=''/>
-                    <Product img='../public/media/IMG_7717.jpg' name=''/>
-                    <Product img='../public/media/IMG_7712.jpg' name=''/>
-                    <Product img='../public/media/IMG_7713.jpg' name=''/>
-                    <Product img='../public/media/IMG_7783.jpg' name=''/>
-                    <Product img='../public/media/IMG_7784.jpg' name=''/>
+                    <Product img='IMG_9687.JPG' name=''/>
+                    <Product img='IMG_7717.JPG' name=''/>
+                    <Product img='IMG_7712.JPG' name=''/>
+                    <Product img='IMG_7713.JPG' name=''/>
+                    <Product img='IMG_7783.JPG' name=''/>
+                    <Product img='IMG_7784.JPG' name=''/>
                 </div>
             </div>
         </div>
