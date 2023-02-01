9909c9da92fca43369b2b8b718fea6a9e40f80cc
fearblackscarab <fearblackscarab@gmail.com>
2023-01-31 22:13:15 -0600

    meta SEO description updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ const Home=()=>{
     return(
         <div className="home">
             <h2 className="test"><a href="" className="text-center d-block story">Our Story</a></h2>
-            <img src="/media/white_dress.png" alt="woman in a white dress displaying a bracelet" className="img-fluid"/>
+            <img src="/media/white_dress.png" alt="woman in a wedding dress displaying examples of Fran riddells handmade jewelry" className="img-fluid"/>
         </div>
     )
 };
