4fc06ae2bd06cd672709cdfc652e0a1e7a07dc14
fearblackscarab <fearblackscarab@gmail.com>
2023-12-05 19:15:35 -0600

    image process updated

--- src/pages/samples/Samples.jsx
+++ src/pages/samples/Samples.jsx
@@ -7,17 +7,29 @@ const Samples=()=>{
             <h2 className="test">Samples</h2>
             <div className="container">
                 <div className="row">
-                    <Product img='sea_necklace.jpeg' name='' type='necklace'/>
-                    <Product img='stopwatch_neck.jpeg' name='' type='necklace'/>
-                    <Product img='seaweed_neck.jpeg' name='' type='necklace'/>
-                    <Product img='emerald_brace.jpeg' name='' type='bracelet'/>
-                    <Product img='emerald_neck.jpeg' name='' type='necklace'/>
+                    <Product img='band_blue.jpg' name='' type='bracelet'/>
+                    <Product img='bland_blue.jpg' name='' type='bracelet'/>
+                    <Product img='blue-man.jpg' name='' type='bracelet'/>
+                    <Product img='book5.jpg' name='' type='bracelet'/>
                     <Product img='broch.jpg' name='' type='bracelet'/>
-                    <Product img='crest.jpg' name='' type='necklace'/>
-                    <Product img='emerald-crest.jpg' name='' type='necklace'/>
-                    <Product img='horse-necklace.jpg' name='' type='necklace'/>
                     <Product img='brochs.jpg' name='' type='bracelet'/>
+                    <Product img='emerald_brace.jpeg' name='' type='bracelet'/>
+                    <Product img='gem_multi5.jpg' name='' type='bracelet'/>
+                    <Product img='multi_strand.jpg' name='' type='bracelet'/>
+                    <Product img='multi6.jpg' name='' type='bracelet'/>
+                    <Product img='multitude.jpg' name='' type='bracelet'/>
+                    <Product img='white_multi2.jpg' name='' type='bracelet'/>
+                    <Product img='white_multi3.jpg' name='' type='bracelet'/>         
+                    <Product img='crest.jpg' name='' type='necklace'/>
+                    <Product img='emerald_neck.jpeg' name='' type='necklace'/>
+                    <Product img='emerald-crest.jpg' name='' type='necklace'/>
+                    <Product img='large_multi5.jpg' name='' type='necklace'/>
+                    <Product img='seaweed_neck.jpeg' name='' type='necklace'/>
+                    <Product img='shield_black.jpg' name='' type='necklace'/>
                     <Product img='star-pendant.jpg' name='' type='necklace'/>
+                    <Product img='stopwatch_neck.jpeg' name='' type='necklace'/>
+                    <Product img='silverware.jpg' name='' type='other'/>
+                    <Product img='star.jpg' name='' type='other'/>
                 </div>
             </div>
         </div>
--- src/parts/Product.jsx
+++ src/parts/Product.jsx
@@ -1,7 +1,7 @@
 const Product = (props) => {
     return (
         <div className="product-card col-12 col-md-4">
-            <img className="product-img img-fluid" src={`/media/${props.img}`} alt={`product samples ${props.type}`} />
+            <img className="product-img img-fluid" src={`/${props.type}/${props.img}`} alt={`product samples ${props.type}`} />
             <h5 className="card-title text-center">{props.name}</h5>
         </div>
     )
